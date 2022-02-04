6caa5a4139a1b09eded1267ae8e75e76326a640c
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-02-04 15:33:22 +0530

    Add files via upload

---
+++ CLASSES/class_3_float.java
@@ -0,0 +1,22 @@
+
+public class class_3_float {
+	
+	int age;
+	String name;
+	long contact_number;
+	void details()
+	{
+		System.out.println("Details are "+ age + " " +name +" "+ contact_number);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    class_3_float obj = new class_3_float();
+	    obj.age=10;
+	    obj.name="Chandu";
+	    obj.contact_number=2344555;
+	    obj.details();
+	
+	}
+
+}
