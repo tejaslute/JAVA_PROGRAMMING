ffee676fb7d573751d39750fec85cb4ed0a28c58
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-02-07 19:16:39 +0530

    Add files via upload

---
+++ PATTERNS/PATTERN_2.java
@@ -0,0 +1,34 @@
+
+public class PATTERN_2 {
+
+	public static void main(String[] args) {
+	
+		/*            *
+		            * * 
+		          * * *
+		        * * * *    
+		         
+		              
+		 */
+
+		
+		
+		for(int i=0;i<4;i++)
+		{
+			for(int j=3;j>i;j--)
+			{
+				System.out.print(" ");
+			}
+			
+			
+			for(int k=0;k<=i;k++)
+			{
+				System.out.print("*");
+			}
+			
+			
+			System.out.println();
+		}
+	}
+
+}
