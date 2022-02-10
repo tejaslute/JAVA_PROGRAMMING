ef9b355101283634c7167d795c31a8c4d316818f
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-02-10 12:39:56 +0530

    Add files via upload

---
+++ PATTERNS/PATTERN_13.java
@@ -0,0 +1,38 @@
+
+public class PATTERN_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   /*
+	    
+	      
+	      * 
+         *         
+       * 
+     * 
+      
+     */
+    	   
+		
+		int rows=4;
+		for(int i=1;i<=rows;i++)
+		{
+		    // For spaces 
+			for(int j=1;j<=rows-i;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			// For star
+			for(int j=1;j<=1;j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+
+	}
+
+}
