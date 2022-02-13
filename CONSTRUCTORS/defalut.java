1b29a2db17a28fb1fde0ccd8d9a54f4f3380e078
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-02-13 10:57:43 +0530

    Add files via upload
    An automatic initlization of an object  is done when instance of an class is created . It get automatically invoked at time of creating . It doesnt has return type  Java doesnt support copy constructor as it passes an refrence no object value is copied .

---
+++ CONSTRUCTORS/defalut.java
@@ -0,0 +1,32 @@
+
+public class defalut {
+    int roo_no;
+    String name;
+     defalut()
+     {
+    	 System.out.println(" Default constructor called ");
+    	 roo_no=10;
+    	 name="Akash";
+     }
+     defalut(int roo_no ,String name)
+     {
+    	 System.out.println(" Parameterized Constructor ");
+    	 this.roo_no=roo_no;
+    	 this.name=name;
+     }
+     void display()
+
+     {
+    	 System.out.println(roo_no + "  "+name);
+     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		defalut sc=new defalut();
+		defalut  s=new defalut(3,"tejas");
+		sc.display();
+		s.display();
+		
+
+	}
+
+}
