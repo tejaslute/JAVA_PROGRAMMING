5fdea1f7863fac77613f0c4151dc8ee7a9179422
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-02-13 15:01:32 +0530

    Add files via upload

---
+++ CONSTRUCTORS/STATIC.java
@@ -0,0 +1,13 @@
+
+public class STATIC {
+static int cube(int x)
+{
+	return x*x*x;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(cube(4));
+
+	}
+
+}
