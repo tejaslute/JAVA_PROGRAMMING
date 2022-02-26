fcf297dc9586218cdfe16abeba4c87babdfa18a5
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2022-02-26 14:27:38 +0530

    Update STATIC.java

--- CONSTRUCTORS/STATIC.java
+++ CONSTRUCTORS/STATIC.java
@@ -5,7 +5,7 @@ static int cube(int x)
 	return x*x*x;
 }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//   TODO Auto-generated method stub
 		System.out.println(cube(4));
 
 	}
