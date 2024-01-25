e45df982c1658e2be2e649a1b83ca8a5a8197340
Tejas Lute <91754694+tejaslute@users.noreply.github.com>
2024-01-25 23:34:51 +0530

    Create inter_thread_communicaion.java

---
+++ THREADING/inter_thread_communicaion.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.lang.*;
+
+class B22 implements Runnable{
+	private String name;
+	
+	B22(String name){
+		this.name=name;
+	}
+	public void run()
+{
+		
+		synchronized(this) {
+		for(int i=0;i<name.length();i++) {
+			System.out.println(name.charAt(i)+" "+Thread.currentThread().getName());
+		}
+		if((Thread.currentThread().getName()).equals("Thread 1 "))
+		{
+		notify();
+		}
+	
+		}
+	
+		
+}
+}
+public class Syring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		B22 b = new B22("Tejas");
+		Thread t1=new Thread(b,"Thread 1 ");
+		Thread t2= new Thread(b,"Thread 2 ");
+		try {
+			
+			synchronized(t1)
+			{
+				t1.start();	
+				t1.wait();
+				 t2.start();
+			  // t2.wait();
+			  
+			
+			   
+				
+			}
+			  
+				
+			
+			
+		
+		
+			
+			
+		}catch(Exception e) {
+			System.out.print(e);
+		}
+		
+	}
+
+}
